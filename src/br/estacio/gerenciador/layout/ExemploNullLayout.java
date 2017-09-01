8f95d97e0cd786ac9f10be46ae29ea30e4ba2a8f
alunoadm <alunoadm@MSCGRESL05008.estacio.acad>
2017-08-31 22:17:18 -0300

    Lista de Layout

---
+++ src/br/estacio/gerenciador/layout/ExemploNullLayout.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.gerenciador.layout;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class ExemploNullLayout extends JFrame {
+
+    private JButton botaoUm, botaoDois, botaoTres;
+
+    public ExemploNullLayout(){
+        Container caixaPrincipal = this.getContentPane();
+        FlowLayout flowLayout = new FlowLayout();
+        this.setLayout(flowLayout);
+        botaoUm = new JButton("Estacio");
+        botaoDois = new JButton("Programação II");
+        botaoTres = new JButton("2017-2");
+        
+        botaoUm.setBounds(10, 10, 150, 140);
+        botaoDois.setBounds(165, 10, 150, 140);
+        botaoTres.setBounds(320, 10, 150, 140);
+        
+        caixaPrincipal.add(botaoUm);
+        caixaPrincipal.add(botaoDois);
+        caixaPrincipal.add(botaoTres);
+        
+        this.setSize(500, 210);
+        this.setVisible(true);
+    }
+}
---
+++ src/br/estacio/gerenciador/layout/TestadoraDeLayout.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.gerenciador.layout;
+
+import br.estacio.gerenciador.layout.ExemploNullLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class TestadoraDeLayout {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame janela = new ExemploNullLayout();
+    }
+    
+}
