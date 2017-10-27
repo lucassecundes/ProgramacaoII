e870647015c9a335f31569e0af764467af1ddb3d
aluno <aluno@MSCGRESL04012.estacio.acad>
2017-10-26 21:18:04 -0300

    Classe de Usuario

---
+++ src/br/estacio/Banco/Usuario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.Banco;
+
+/**
+ *
+ * @author aluno
+ */
+public class Usuario {
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+    
+}
